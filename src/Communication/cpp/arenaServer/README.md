e9fbf940b9c6a7c87d0898fedc05e36bb5fe8a8e
AMLing2 <mathias.lervik@hotmail.com>
2025-03-19 21:43:05 +0100

    fixed compilation issue

---
+++ src/Communication/cpp/arenaServer/README.md
@@ -0,0 +1,16 @@
+# Drone arena Server
+
+## Compilation Instructions
+1. Run the *genProto.sh* bash script to generate the protobuf files
+
+2. **Create Build Directory**
+   ```bash
+   mkdir build
+   cd build
+    ```
+3. configure cmake and compile
+   ```bash
+   cmake ..
+   make
+    ```
+
--- src/Communication/genProto.sh
+++ src/Communication/genProto.sh
@@ -1,3 +1,4 @@
 echo "generating protobuf files"
-protoc -I=. --python_out=. --python_out=../Camera/comm_module --python_out=../RL/Modules ./dronePosVec.proto
-protoc -I=. --cpp_out=../Drone/droneIMU/src --cpp_out=./cpp/arenaServer/src ./dronePosVec.proto
+#protoc -I=. --python_out=. --python_out=../Camera/comm_module --python_out=../RL/Modules ./dronePosVec.proto
+#protoc -I=. --cpp_out=../Drone/droneIMU/src --cpp_out=./cpp/arenaServer/src ./dronePosVec.proto
+protoc -I=. --cpp_out=./cpp/arenaServer/src ./dronePosVec.proto
